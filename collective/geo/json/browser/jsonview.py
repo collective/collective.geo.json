d2f4ec8fb96d2972805f6bfae0c8b0b920610715
Benoît Suttor <benoit.suttor@imio.be>
2014-07-30 16:13:24 +0200

    add plone.app.collection view

--- collective/geo/json/browser/jsonview.py
+++ collective/geo/json/browser/jsonview.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from zope.interface import implements, Interface
 from zope.component import getUtility
 
@@ -17,6 +18,7 @@ from plone.registry.interfaces import IRegistry
 from collective.geo.geographer.interfaces import IGeoreferenced
 from collective.geo.settings.interfaces import IGeoFeatureStyle
 
+
 def get_marker_image(context, marker_img):
     try:
         marker_img = Expression(str(marker_img))(getExprContext(context))
@@ -35,7 +37,6 @@ class JsonBaseDocument(BrowserView):
     defaultstyles = None
     styles = None
 
-
     def __init__(self, context, request):
         self.context = context
         self.request = request
@@ -51,13 +52,13 @@ class JsonBaseDocument(BrowserView):
             if color.startswith('#'):
                 color = color[1:]
             tc = int(color, 16)
-            if len(color)==3:
-                color =''.join([b*2 for b in color]) +'3c'
-            elif len(color)==4:
-                color =''.join([b*2 for b in color])
-            elif len(color)==6:
-                color = color +'3c'
-            elif len(color)==8:
+            if len(color) == 3:
+                color = ''.join([b*2 for b in color]) + '3c'
+            elif len(color) == 4:
+                color = ''.join([b*2 for b in color])
+            elif len(color) == 6:
+                color = color + '3c'
+            elif len(color) == 8:
                 pass
             else:
                 raise ValueError, "input #%s is not in #RRGGBB[AA] format" % color
@@ -65,29 +66,28 @@ class JsonBaseDocument(BrowserView):
         return 'AABBCCDD'
 
     def _get_style(self, geo_type):
-        style= {}
+        style = {}
         if self.styles:
             fill = self.normalize_color(self.styles['polygoncolor'])
             stroke = self.normalize_color(self.styles['linecolor'])
-            if self.styles['use_custom_styles']:
-                if geo_type['type'].endswith('Polygon'):
-                    style['fill'] = fill
-                    style['stroke'] = stroke
-                    style['width'] = self.styles['linewidth']
-                elif geo_type['type'].endswith('LineString'):
-                    style['stroke'] = stroke
-                    style['width'] = self.styles['linewidth']
-                elif geo_type['type'].endswith('Point'):
-                    self.styles['marker_image']
-                    style['fill'] = fill
-                    style['stroke'] =stroke
-                    style['width'] = self.styles['linewidth']
-                    if self.styles.get('marker_image', None):
-                        img = get_marker_image( self.context,
-                                self.styles['marker_image'])
-                        style['image']= img
-                    else:
-                        style['image'] = None
+            if geo_type['type'].endswith('Polygon'):
+                style['fill'] = fill
+                style['stroke'] = stroke
+                style['width'] = self.styles['linewidth']
+            elif geo_type['type'].endswith('LineString'):
+                style['stroke'] = stroke
+                style['width'] = self.styles['linewidth']
+            elif geo_type['type'].endswith('Point'):
+                self.styles['marker_image']
+                style['fill'] = fill
+                style['stroke'] = stroke
+                style['width'] = self.styles['linewidth']
+                if self.styles.get('marker_image', None):
+                    img = get_marker_image(self.context,
+                                           self.styles['marker_image'])
+                    style['image'] = img
+                else:
+                    style['image'] = None
         return style
 
     @property
@@ -97,11 +97,9 @@ class JsonBaseDocument(BrowserView):
 
 class JsonDocument(JsonBaseDocument):
 
-
-
-
     def __call__(self):
-        self.request.RESPONSE.setHeader('Content-Type','application/json; charset=utf-8')
+        self.request.RESPONSE.setHeader('Content-Type',
+                                        'application/json; charset=utf-8')
         try:
             geometry = IGeoreferenced(self.context)
         except:
@@ -114,55 +112,67 @@ class JsonDocument(JsonBaseDocument):
             classes = geometry.geo['type'].lower() + ' '
         else:
             classes = ''
-        classes += self.context.getPhysicalPath()[-2].replace('.','-') #+ ' '
+        classes += self.context.getPhysicalPath()[-2].replace('.', '-')
         json_result = [
-                geojson.Feature(
-                    id=self.context.id.replace('.','-'),
-                    geometry=geometry.geo,
-                    properties={
-                        "title": self.context.Title(),
-                        "description": self.context.Description(),
-                        "style": self._get_style(geometry.geo),
-                        "url": self.context.absolute_url(),
-                        "classes" : classes,
-                        })]
+            geojson.Feature(
+                id=self.context.id.replace('.', '-'),
+                geometry=geometry.geo,
+                properties={
+                    "title": self.context.Title(),
+                    "description": self.context.Description(),
+                    "style": self._get_style(geometry.geo),
+                    "url": self.context.absolute_url(),
+                    "classes": classes,
+                    })]
         return geojson.dumps(geojson.FeatureCollection(json_result))
+
 
 class JsonFolderDocument(JsonBaseDocument):
 
     def get_brain(self):
-        return self.context.getFolderContents()
+        querydict = {}
+        querydict['path'] = {'query': '/'.join(self.context.getPhysicalPath())}
+        return self.portal_catalog(querydict)
 
     def __call__(self):
         json_result = []
         for brain in self.get_brain():
             if brain.zgeo_geometry:
                 self.styles = brain.collective_geo_styles
-                geom = { 'type': brain.zgeo_geometry['type'],
-                            'coordinates': brain.zgeo_geometry['coordinates']}
+                geom = {'type': brain.zgeo_geometry['type'],
+                        'coordinates': brain.zgeo_geometry['coordinates']}
                 if geom['coordinates']:
-                    if geometry.geo['type']:
-                        classes = geometry.geo['type'].lower() + ' '
+                    if geom['type']:
+                        classes = geom['type'].lower() + ' '
                     else:
                         classes = ''
-                    classes += brain.getPath().split('/')[-2].replace('.','-')
+                    classes += brain.getPath().split('/')[-2].replace('.', '-')
                     json_result.append(
-                         geojson.Feature(
-                            id=brain.id.replace('.','-'),
+                        geojson.Feature(
+                            id=brain.id.replace('.', '-'),
                             geometry=asShape(geom),
-                            style= self._get_style(geom),
+                            style=self._get_style(geom),
                             properties={
                                 "title": brain.Title,
                                 "description": brain.Description,
                                 "style": self._get_style(geom),
                                 "url": brain.getURL(),
-                                "classes" : classes,
+                                "classes": classes,
                                 }))
-        self.request.RESPONSE.setHeader('Content-Type','application/json; charset=utf-8')
-        return geojson.dumps(geojson.FeatureCollection(json_result))
+        self.request.RESPONSE.setHeader('Content-Type',
+                                        'application/json; charset=utf-8')
+        feature_collection = geojson.FeatureCollection(json_result)
+        feature_collection.update({'title': self.context.title})
+        return geojson.dumps(feature_collection)
+
 
 class JsonTopicDocument(JsonFolderDocument):
 
     def get_brain(self):
         return self.context.queryCatalog()
 
+
+class JsonCollectionDocument(JsonTopicDocument):
+
+    def get_brain(self):
+        return self.context.queryCatalog()
